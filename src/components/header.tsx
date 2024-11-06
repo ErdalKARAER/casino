1079eb474e84bae71865507c1c05cdc076718b79
Erdal Karaer <karaer.erd@gmail.com>
2024-11-06 21:21:10 +0100

    creating header

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+const Header = () => (
+  <header className="flex justify-evenly items-center">
+    <Button>Home</Button>
+    <Button>Blackjack</Button>
+    <Button>Roulette</Button>
+  </header>
+)
+
+export default Header
